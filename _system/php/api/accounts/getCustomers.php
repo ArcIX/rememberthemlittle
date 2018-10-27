cf8804f2ab8f4931c4ce3843e4d4fc5fea981703
ArcIX <rc2582739@gmail.com>
2018-10-27 19:34:02 +0800

    Commen about future modification on accounts getCustomers

--- _system/php/api/accounts/getCustomers.php
+++ _system/php/api/accounts/getCustomers.php
@@ -17,6 +17,12 @@ if (true);
         # Perform SQL Query
         # Desired output: Account Name | Number of Bookings Made Since The Beginning | Total Cash Paid To RTL | Latest Pictorial Session | Next Pictorial Session
         $sql = "SELECT a.account_name, COUNT(b.account_id) AS number_of_bookings, SUM(b.booking_total_price) AS total_expediture FROM accounts a LEFT JOIN bookings b ON a.account_id = b.account_id GROUP BY a.account_id";
+        
+        # Get latest booking of each customer
+        # $sql = "SELECT MAX(b.booking_date) FROM accounts a LEFT JOIN bookings b WHERE b.booking_date < GETDATE() GROUP BY a.account_id"
+        # Get next booking of each customer
+        # $sql = "SELECT MIN(b.booking_date) FROM accounts a LEFT JOIN bookings b WHERE b.booking_date > GETDATE() GROUP BY a.account_id";
+        
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         
