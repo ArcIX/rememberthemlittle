5e2467df6f5611c09938a7fb03d7dd83e8c68810
ArcIX <rc2582739@gmail.com>
2018-10-30 15:58:25 +0800

    Add bookings getByBookingDate

---
+++ _system/php/api/bookings/getByBookingDate.php
@@ -0,0 +1,41 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";  
+$username = "root";
+$password = "mysql";
+
+# POST parameters
+$booking_date = $_POST['booking_date'];
+
+if (isset($booking_date))
+{
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        # Perform SQL Query
+        $stmt = $conn->prepare("SELECT * FROM bookings WHERE booking_date = $booking_date ORDER BY booking_date ASC");
+        $stmt->execute();
+    
+        # Fetch Result
+        $result = $stmt->fetchAll();
+        
+         # Print Result in JSON Format
+         echo json_encode((object)[
+            'success' => true,
+            'data' => $result
+         ],JSON_NUMERIC_CHECK);
+    
+        }
+    catch(PDOException $e)
+        {
+            echo json_encode((object)[
+                'success' => false,
+                'message' => "Connection failed: " . $e->getMessage()
+            ]);
+        }
+}
+?>
