48cbf04f093e1ff04760241fff9cf34fd1612208
ArcIX <rc2582739@gmail.com>
2018-10-30 15:58:05 +0800

    Very minor edits to getOpenSlots

--- _system/php/api/bookings/getOpenSlots.php
+++ _system/php/api/bookings/getOpenSlots.php
@@ -18,12 +18,15 @@ if (isset($date) && isset($duration))
         # Connect to Database
         $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         # Perform SQL Query
         $sql = "SELECT b.booking_id, b.booking_date, b.booking_time, p.package_minutes FROM bookings b INNER JOIN packages p ON b.booking_date = '$date' AND b.package_id = p.package_id ORDER BY b.booking_time ASC";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
+
         # Fetch Result
         $result = $stmt->fetchAll();
+        
         // print_r($result);
         $start = strtotime("8:00"); #opening time
         $stop = strtotime("20:00"); #closing time
